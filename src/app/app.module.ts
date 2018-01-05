b43f71ece5edadd731553f76262ee1195e219699
shetz163 <shetz163@qq.com>
2018-01-06 03:12:55 +0800

    chroe. wtf about the super tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { OrderWindowPage } from '../pages/order-window/order-window';
 import { ChatPage } from '../pages/chat/chat';
 import { TradePage } from '../pages/trade/trade';
-import { TradeSellEthereumPage } from '../pages/trade-sell-ethereum/trade-sell-ethereum';
-import { TradeSellMoneroPage } from '../pages/trade-sell-monero/trade-sell-monero';
-import { TradeSellRipplePage } from '../pages/trade-sell-ripple/trade-sell-ripple';
-import { TradeSellStellarPage } from '../pages/trade-sell-stellar/trade-sell-stellar';
-import { TradeBuyEthereumPage } from '../pages/trade-buy-ethereum/trade-buy-ethereum';
-import { TradeBuyMoneroPage } from '../pages/trade-buy-monero/trade-buy-monero';
-import { TradeBuyRipplePage } from '../pages/trade-buy-ripple/trade-buy-ripple';
-import { TradeBuyStellarPage } from '../pages/trade-buy-stellar/trade-buy-stellar';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SuperTabsModule } from 'ionic2-super-tabs'
@@ -33,14 +24,6 @@ import { SuperTabsModule } from 'ionic2-super-tabs'
     OrderWindowPage,
     ChatPage,
     TradePage,
-    TradeSellEthereumPage,
-    TradeSellMoneroPage,
-    TradeSellRipplePage,
-    TradeSellStellarPage,
-    TradeBuyEthereumPage,
-    TradeBuyMoneroPage,
-    TradeBuyRipplePage,
-    TradeBuyStellarPage
   ],
   imports: [
     BrowserModule,
@@ -57,14 +40,6 @@ import { SuperTabsModule } from 'ionic2-super-tabs'
     OrderWindowPage,
     ChatPage,
     TradePage,
-    TradeSellEthereumPage,
-    TradeSellMoneroPage,
-    TradeSellRipplePage,
-    TradeSellStellarPage,
-    TradeBuyEthereumPage,
-    TradeBuyMoneroPage,
-    TradeBuyRipplePage,
-    TradeBuyStellarPage
   ],
   providers: [
     StatusBar,
--- src/pages/trade/trade.ts
+++ src/pages/trade/trade.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { TradeSellEthereumPage } from '../trade-sell-ethereum/trade-sell-ethereum';
-import { TradeSellMoneroPage } from '../trade-sell-monero/trade-sell-monero';
-import { TradeSellRipplePage } from '../trade-sell-ripple/trade-sell-ripple';
-import { TradeSellStellarPage } from '../trade-sell-stellar/trade-sell-stellar';
-import { TradeBuyEthereumPage } from '../trade-buy-ethereum/trade-buy-ethereum';
-import { TradeBuyMoneroPage } from '../trade-buy-monero/trade-buy-monero';
-import { TradeBuyRipplePage } from '../trade-buy-ripple/trade-buy-ripple';
-import { TradeBuyStellarPage } from '../trade-buy-stellar/trade-buy-stellar';
+// import { TradeSellEthereumPage } from '../trade-sell-ethereum/trade-sell-ethereum';
+// import { TradeSellMoneroPage } from '../trade-sell-monero/trade-sell-monero';
+// import { TradeSellRipplePage } from '../trade-sell-ripple/trade-sell-ripple';
+// import { TradeSellStellarPage } from '../trade-sell-stellar/trade-sell-stellar';
+// import { TradeBuyEthereumPage } from '../trade-buy-ethereum/trade-buy-ethereum';
+// import { TradeBuyMoneroPage } from '../trade-buy-monero/trade-buy-monero';
+// import { TradeBuyRipplePage } from '../trade-buy-ripple/trade-buy-ripple';
+// import { TradeBuyStellarPage } from '../trade-buy-stellar/trade-buy-stellar';
 
 /**
  * Generated class for the TradePage page.
@@ -24,36 +24,36 @@ import { TradeBuyStellarPage } from '../trade-buy-stellar/trade-buy-stellar';
 export class TradePage {
   buynsell: string = "buy";
   buycryptos: Object[] = [{
-    root: TradeBuyEthereumPage,
+    root: 'TradeBuyEthereumPage',
     title: 'Ethereum',
     icon: 'eth'
   },{
-    root: TradeBuyMoneroPage,
+    root: 'TradeBuyMoneroPage',
     title: 'Monero',
     icon: 'monero'
   },{
-    root: TradeBuyRipplePage,
+    root: 'TradeBuyRipplePage',
     title: 'Ripple',
     icon: 'ripple'
   },{
-    root: TradeBuyStellarPage,
+    root: 'TradeBuyStellarPage',
     title: 'Stellar',
     icon: 'stellar'
   }]
   sellcryptos: Object[] = [{
-    root: TradeSellEthereumPage,
+    root: 'TradeSellEthereumPage',
     title: 'Ethereum',
     icon: 'eth'
   },{
-    root: TradeSellMoneroPage,
+    root: 'TradeSellMoneroPage',
     title: 'Monero',
     icon: 'monero'
   },{
-    root: TradeSellRipplePage,
+    root: 'TradeSellRipplePage',
     title: 'Ripple',
     icon: 'ripple'
   },{
-    root: TradeSellStellarPage,
+    root: 'TradeSellStellarPage',
     title: 'Stellar',
     icon: 'stellar'
   }]
