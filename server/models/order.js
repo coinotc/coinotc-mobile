8d3cb241a1e968d0c891cb78ff473d204b06cc52
Louis <louishwy@heweiyongdeMacBook-Pro.local>
2018-01-05 17:13:27 +0800

    fix mongoose

---
+++ server/models/order.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('order', {
+    buyer: String,
+    seller: String,
+    crypto: String,
+    country: String,
+    fiat: String,
+    price: Number,
+    amount: Number,
+    quantity: Number,
+    payment: String,
+    advertisement: String,
+    finished: Boolean
+});
---
+++ server/routes/api/order-routes.js
@@ -0,0 +1,20 @@
+var Order = require('../../models/order');
+
+const ORDERS_API_URL = '/api/v1/orders';
+
+module.exports = function (app) {
+
+    //SEARCH & GET PROJECTS
+    app.get(ORDERS_API_URL, (req, res) => {
+        var query = {};
+        Order.find(query, (err, orders) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err);
+                // return;
+            }
+            res.status(200).json(orders);
+        });
+    });
+
+}
