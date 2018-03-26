c0d23814f1434d3aa787c0d087cac3d0ee58e9ee
louishwy <905615218@qq.com>
2018-03-26 15:33:18 +0800

    fix message in orders

--- src/models/adinformation.ts
+++ src/models/adinformation.ts
@@ -1,17 +1,15 @@
-export class adinformation{
-    constructor(
-        public owner: string,
-        public visible: Boolean,
-        public crypto: string,
-        public country: string,
-        public fiat: string,
-        public price: number,
-        public min_price: number,
-        public max_price: number,
-        public payment: string,
-        public limit: number,
-        public massage: string
-    ){
-
-    }
+export class adinformation {
+  constructor(
+    public owner: string,
+    public visible: Boolean,
+    public crypto: string,
+    public country: string,
+    public fiat: string,
+    public price: number,
+    public min_price: number,
+    public max_price: number,
+    public payment: string,
+    public limit: number,
+    public message: string
+  ) {}
 }
