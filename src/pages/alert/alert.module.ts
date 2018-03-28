74e62840dc8333eda6b68f318063148636692084
louishwy <905615218@qq.com>
2018-03-28 18:01:32 +0800

    translate alert

--- src/pages/alert/alert.module.ts
+++ src/pages/alert/alert.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AlertPage } from './alert';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [
-    AlertPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(AlertPage),
-  ],
+  declarations: [AlertPage],
+  imports: [IonicPageModule.forChild(AlertPage), TranslateModule.forChild()]
 })
 export class AlertPageModule {}
