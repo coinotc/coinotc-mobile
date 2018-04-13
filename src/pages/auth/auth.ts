6734d41c81e37091ec342c1877225e4f6e14206c
louishwy <905615218@qq.com>
2018-04-13 17:56:06 +0800

    update deviceToken when login

--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -12,12 +12,13 @@ import {
   FormBuilder,
   FormGroup,
   FormControl,
-  Validators,
+  Validators
 } from '@angular/forms';
 import { UserServiceProvider } from '../../providers/user-service/user-service';
+import { ProfileServiceProvider } from '../../providers/profile-service/profile-service';
 import { Errors } from '../../models/errors.model';
 import { TabsPage } from '../../pages/tabs/tabs';
-import { PincodePage } from '../pincode/pincode'
+import { PincodePage } from '../pincode/pincode';
 import { FCM, NotificationData } from '@ionic-native/fcm';
 import { Platform } from 'ionic-angular';
 import { Network } from '@ionic-native/network';
@@ -43,55 +44,55 @@ export class AuthPage {
   password = 'password';
   onlineToast: any;
   offlineToast: any;
-  
+
   constructor(
     public navCtrl: NavController,
     private viewCtrl: ViewController,
     private toastCtrl: ToastController,
     private userService: UserServiceProvider,
+    private profileServiceProvider: ProfileServiceProvider,
     private params: NavParams,
     private fb: FormBuilder,
     private fcm: FCM,
     private platform: Platform,
     private network: Network,
     private loadingCtrl: LoadingController,
-    public appCtrl:App
+    public appCtrl: App
   ) {
     // use FormBuilder to create a form group
-      this.authForm = this.fb.group({
-        email: ['', Validators.required],
-        password: ['', Validators.required]
-      });
+    this.authForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
     this.isModal = !!params.get('isModal');
     this.platform.ready().then(() => {
-            this.fcm
-              .getToken()
-              .then((token: string) => {
-                console.log('The token to use is: ', token);
-                this.deviceToken = token;
-              })
-              .catch(error => {
-                console.error(error);
-              });
-            this.fcm.onTokenRefresh().subscribe(token => {
-              console.log(token);
-              this.deviceToken = token;
-             });
-            this.fcm.onNotification().subscribe(
-              (data: NotificationData) => {
-                if (data.wasTapped) {
-                  console.log('Received in background', JSON.stringify(data));
-                } else {
-                  console.log('Received in foreground', JSON.stringify(data));
-                }
-              },
-              error => {
-                console.error('Error in notification', error);
-              }
-            );
-          });
+      this.fcm
+        .getToken()
+        .then((token: string) => {
+          console.log('The token to use is: ', token);
+          this.deviceToken = token;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+      this.fcm.onTokenRefresh().subscribe(token => {
+        console.log(token);
+        this.deviceToken = token;
+      });
+      this.fcm.onNotification().subscribe(
+        (data: NotificationData) => {
+          if (data.wasTapped) {
+            console.log('Received in background', JSON.stringify(data));
+          } else {
+            console.log('Received in foreground', JSON.stringify(data));
+          }
+        },
+        error => {
+          console.error('Error in notification', error);
+        }
+      );
+    });
   }
-
 
   matchValidator = (control: FormControl): { [s: string]: boolean } => {
     if (!control.value) {
@@ -103,24 +104,23 @@ export class AuthPage {
     }
   };
 
-
   authTypeChange() {
     if (this.authType === 'register') {
       this.authForm.addControl('username', new FormControl());
     } else {
       this.authForm.removeControl('username');
     }
-    if(this.authType === 'login'){
+    if (this.authType === 'login') {
       this.authForm = this.fb.group({
         email: ['', Validators.required],
         password: ['', Validators.required]
       });
-    }else{
+    } else {
       this.authForm = this.fb.group({
-        username :['', Validators.required],
-        email: ['', [Validators.required,this.emailValidator]],
+        username: ['', Validators.required],
+        email: ['', [Validators.required, this.emailValidator]],
         password: ['', Validators.required],
-        confirmPassword: ['',[Validators.required,this.equals(this.password)]]
+        confirmPassword: ['', [Validators.required, this.equals(this.password)]]
         //confirmPassword: ['',[this.matchValidator]]
       });
     }
@@ -128,82 +128,92 @@ export class AuthPage {
   emailValidator = (control: FormControl): { [s: string]: boolean } => {
     const EMAIL_REGEXP = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     if (!control.value) {
-      return { required: true }
+      return { required: true };
     } else if (!EMAIL_REGEXP.test(control.value)) {
       return { error: true, email: true };
     }
   };
-  equals(fieldName: string){
+  equals(fieldName: string) {
     let fcfirst: FormControl;
     let fcSecond: FormControl;
     return function matchValidator(control: FormControl) {
-        if (!control.parent) {
-            return null;
-        }
-        // INITIALIZING THE VALIDATOR.
-        if (!fcfirst) {
-            //INITIALIZING FormControl first
-            fcfirst = control;
-            fcSecond = control.parent.get("password") as FormControl;
-            //FormControl Second
-            if (!fcSecond) {
-                throw new Error('matchValidator(): Second control is not found in the parent group!');
-            }
-            fcSecond.valueChanges.subscribe(() => {
-                fcfirst.updateValueAndValidity();
-            });
-        }
-        if (!fcSecond) {
-            return null;
-        }
-        if (fcSecond.value !== fcfirst.value) {
-            return {
-                matchOther: true
-            };
-        }
+      if (!control.parent) {
         return null;
-    }
-}
-  
+      }
+      // INITIALIZING THE VALIDATOR.
+      if (!fcfirst) {
+        //INITIALIZING FormControl first
+        fcfirst = control;
+        fcSecond = control.parent.get('password') as FormControl;
+        //FormControl Second
+        if (!fcSecond) {
+          throw new Error(
+            'matchValidator(): Second control is not found in the parent group!'
+          );
+        }
+        fcSecond.valueChanges.subscribe(() => {
+          fcfirst.updateValueAndValidity();
+        });
+      }
+      if (!fcSecond) {
+        return null;
+      }
+      if (fcSecond.value !== fcfirst.value) {
+        return {
+          matchOther: true
+        };
+      }
+      return null;
+    };
+  }
+
   submitForm() {
     let loading = this.loadingCtrl.create({
-        spinner: 'circles',
-        content: 'loading...',
-        duration: 3000
-
+      spinner: 'circles',
+      content: 'loading...',
+      duration: 3000
     });
     loading.present();
     console.log(this.deviceToken);
     this.isSubmitting = true;
     const credentials = this.authForm.value;
     //this.navCtrl.push(TabsPage,{});
-    
+
     //console.log(this.navCtrl.parent);
     this.userService
       .attemptAuth(this.authType, credentials, this.deviceToken)
       .subscribe(
         user => {
-          console.log("subscribe user!!!");
+          console.log('subscribe user!!!');
           if (this.isModal) this.viewCtrl.dismiss();
           this.displayTabs();
           if (this.authType === 'register') {
             this.appCtrl.getRootNav().setRoot(PincodePage);
           } else {
-            console.log("Login ...." + this.navCtrl.parent);
+            console.log('Login ....' + this.navCtrl.parent);
+            let updater = this.userService.getCurrentUser().username;
+            this.profileServiceProvider
+              .updateDeviceToken(updater, this.deviceToken)
+              .subscribe(result => {
+                console.log('...update deviceToken successfully...');
+              });
             /*
             setTimeout(() => {
               this.appCtrl.getRootNav().setRoot(TabsPage);
             }, 1000);*/
-            loading.dismiss().then(()=>{
-              if(this.navCtrl.parent != null){
-                console.log(">>>>"+ this.navCtrl.parent)
-                this.navCtrl.parent.previousTab(false)
-                this.navCtrl.parent.select(0);
-              }
-              this.appCtrl.getRootNav().setRoot(TabsPage);
-            }).catch(e=> console.log(e));
+            loading
+              .dismiss()
+              .then(() => {
+                if (this.navCtrl.parent != null) {
+                  console.log('>>>>' + this.navCtrl.parent);
+                  this.navCtrl.parent.previousTab(false);
+                  this.navCtrl.parent.select(0);
+                }
+                this.appCtrl.getRootNav().setRoot(TabsPage);
+              })
+              .catch(e => console.log(e));
             //setTimeout(() => {
-              
+
             //}, 2500);
             //this.navCtrl.parent.previousTab(false)
           }
@@ -220,8 +230,7 @@ export class AuthPage {
           this.isSubmitting = false;
         }
       );
-    
-    
+
     // }else{
     //   let toast = this.toastCtrl.create({
     //     message: 'Wrong type',
@@ -232,7 +241,6 @@ export class AuthPage {
     //   });
     //   toast.present();
     // }
-
   }
 
   private displayTabs() {
@@ -249,42 +257,48 @@ export class AuthPage {
     this.viewCtrl.dismiss();
   }
 
-  displayOnlineNetworkUpdate(connectionState: string){
+  displayOnlineNetworkUpdate(connectionState: string) {
     let networkType = this.network.type;
     this.networkStatusIndicator = 2;
     //this.offlineToast.dismiss();
     this.onlineToast = this.toastCtrl
-        .create({
-          message: `You are now ${connectionState} via ${networkType}`,
-          duration: 3000
-        }).present();
+      .create({
+        message: `You are now ${connectionState} via ${networkType}`,
+        duration: 3000
+      })
+      .present();
   }
 
-  displayOfflineNetworkUpdate(connectionState: string){
+  displayOfflineNetworkUpdate(connectionState: string) {
     this.networkStatusIndicator = 1;
     //this.onlineToast.dismiss();
     this.offlineToast = this.toastCtrl
-        .create({
-          message: `You are now offline`,
-          showCloseButton: true
-        }).present();
+      .create({
+        message: `You are now offline`,
+        showCloseButton: true
+      })
+      .present();
   }
 
   ionViewDidEnter() {
-    this.network.onConnect().subscribe(data => {
-      console.log(data);
-      if(this.networkStatusIndicator != 2){
-        this.displayOnlineNetworkUpdate(data.type);
-      }
-    }, error => console.error(error));
-   
-    this.network.onDisconnect().subscribe(data => {
-      console.log(data);
-      if(this.networkStatusIndicator != 1){
-        this.displayOfflineNetworkUpdate(data.type);
-      }
-    }, error => console.error(error));
+    this.network.onConnect().subscribe(
+      data => {
+        console.log(data);
+        if (this.networkStatusIndicator != 2) {
+          this.displayOnlineNetworkUpdate(data.type);
+        }
+      },
+      error => console.error(error)
+    );
+
+    this.network.onDisconnect().subscribe(
+      data => {
+        console.log(data);
+        if (this.networkStatusIndicator != 1) {
+          this.displayOfflineNetworkUpdate(data.type);
+        }
+      },
+      error => console.error(error)
+    );
   }
-
 }
-
