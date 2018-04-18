57416bdd197e8b1ff074245484d7edec731b6a16
Kenneth Phang <bunnyppl@gmail.com>
2018-04-19 03:09:31 +0800

    fix: clean up

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { TextAvatarDirective } from './text-avatar/text-avatar';
+import { ShowHideInput } from './ShowHideInput/show-hiden-input';
+
 @NgModule({
-	declarations: [TextAvatarDirective],
+	declarations: [TextAvatarDirective, ShowHideInput],
 	imports: [],
-	exports: [TextAvatarDirective]
+	exports: [TextAvatarDirective, ShowHideInput]
 })
 export class DirectivesModule {}
