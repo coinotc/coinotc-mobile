94e60cd26e8794096d0bfb495086c9e2b2f37766
louishwy <905615218@qq.com>
2018-05-17 15:22:42 +0800

    feat translate settings

--- src/pages/modify-password/modify-password.module.ts
+++ src/pages/modify-password/modify-password.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ModifyPasswordPage } from './modify-password';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [
-    ModifyPasswordPage,
-  ],
+  declarations: [ModifyPasswordPage],
   imports: [
     IonicPageModule.forChild(ModifyPasswordPage),
-  ],
+    TranslateModule.forChild()
+  ]
 })
 export class ModifyPasswordPageModule {}
