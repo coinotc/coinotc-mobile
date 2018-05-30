4e2afc37f11feb470474c65c3b5abf686b3c15f3
Mr-Mooo <hjchoujiacheng@126.com>
2018-05-30 14:31:36 +0800

    add system message in room page &fix view my advertisement

---
+++ src/pages/view-my-advertisement/view-my-advertisement.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ViewMyAdvertisementPage } from './view-my-advertisement';
+
+@NgModule({
+  declarations: [
+    ViewMyAdvertisementPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ViewMyAdvertisementPage),
+  ],
+})
+export class ViewMyAdvertisementPageModule {}
