de53c4aa7468b909372f528ae404a7c132a9c9c5
Kenneth Phang <bunnyppl@gmail.com>
2018-07-02 14:44:57 +0800

    fix: icons

--- src/pages/wallet/wallet.ts
+++ src/pages/wallet/wallet.ts
@@ -97,12 +97,24 @@ export class WalletPage implements OnInit{
   }
 
   scan(){
-    this.barcodeScanner.scan().then(barcodeData => {
-      console.log('Barcode data', barcodeData);
-      this.scannedText = barcodeData.text;
-     }).catch(err => {
-         console.log('Error', err);
-     });
+    this.barcodeScanner.scan({
+        preferFrontCamera : false, // iOS and Android
+        showFlipCameraButton : true, // iOS and Android
+        showTorchButton : true, // iOS and Android
+        torchOn: false, // Android, launch with the torch switched on (if available)
+        prompt : "Place a barcode inside the scan area", // Android
+        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+        orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+        disableAnimations : true, // iOS
+        disableSuccessBeep: true // iOS and Android
+    }).then(barcodeData => {
+        console.log('Barcode data', barcodeData);
+        this.scannedText = barcodeData.text;
+    }).catch(err => {
+        console.log('Error', err);
+    });
+     
   }
 
   balance(id, type) {
